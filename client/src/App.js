b59fc2f4f6fbf75cb2e0eae5d4058f27210c24c1
n1stodev <nurillotojiakhmedov@gmail.com>
2023-04-06 20:42:44 +0500

    nurillo

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <Navbar />
+      <Slider />
+      <Footer />
     </div>
   );
 }
