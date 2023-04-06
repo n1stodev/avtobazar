bea1e7bc5bbd01681be04cbb9b1e26f05758bf6d
n1stodev <nurillotojiakhmedov@gmail.com>
2023-04-06 20:52:16 +0500

    dadads

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Slider from './components/slider/Slider';
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
