a829fda9e97ebb44aa33c1e5152ee96ce680ea3e
n1stodev <nurillotojiakhmedov@gmail.com>
2023-04-10 00:37:45 +0500

    nurillo

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Navbar.css'
+import { NavLink, Link } from 'react-router-dom'
+import { FiMessageCircle, FiHeart, FiUser } from 'react-icons/fi'
+import { FaTruckMoving, FaMotorcycle, FaCarSide, FaBars } from 'react-icons/fa'
+import { BsXLg } from 'react-icons/bs'
 
 function Navbar() {
+    const [open, setOpen] = useState(false)
+    const [nav, setNav] = useState(false)
     return (
-        <div>
-            <h2 className="nav__logo">AutoBazar</h2>
-            <div className="nav__collection">
-                <button className="btn"></button>
+        <div className='navbar'>
+            <div className="container">
+                <h2 className="nav__logo">Auto<span>Bazar</span> </h2>
+                <div className={`nav__collection ${nav ? 'active' : 'unactive'}`}>
+                    <button className="nav__btn" onClick={() => setOpen(!open)}>Katalog</button>
+                    <ul className="nav__col">
+                        <NavLink to='/'>
+                            <FiMessageCircle />
+                            <span>Xabarlar</span>
+                        </NavLink>
+                        <NavLink to='/'>
+                            <FiHeart />
+                            <span>Yoqtirilganlar</span>
+                        </NavLink>
+                        <NavLink to='/'>
+                            <FiUser />
+                            <span>Kirish</span>
+                        </NavLink>
+                    </ul>
+                </div>
+                <div className={`nav__dropdown ${open ? 'active' : 'unactive'}`}>
+                    <h2>Katalog</h2>
+                    <Link to='/' classsName='nav__dropdownItem'>
+                        <FaCarSide />
+                        <span>Yengil mashinalar</span>
+                    </Link>
+                    <Link to='/' classsName='nav__dropdownItem'>
+                        <FaMotorcycle />
+                        <span>Mototsikllar</span>
+                    </Link>
+                    <Link to='/' classsName='nav__dropdownItem'>
+                        <FaTruckMoving />
+                        <span>Yuk mashinalar</span>
+                    </Link>
+                </div>
+                <button onClick={() => setNav(!nav)} className="nav__btn">{nav ? <FaBars /> : <BsXLg />}</button>
             </div>
         </div>
     )
--- client/src/components/slider/Slider.js
+++ client/src/components/slider/Slider.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs'
+import { CAROUSEL } from '../../static'
+import './Slider.css'
+
+console.log(CAROUSEL)
 
 function Slider() {
+    const [curInx, setCurInx] = useState(0)
+    const prevSlide = () => {
+        const isFirstSlide = curInx === 0
+        const newInx = isFirstSlide ? CAROUSEL.length - 1 : curInx - 1
+        setCurInx(newInx)
+    }
+
+    const nextSlide = () => {
+        const isLastSlide = curInx === CAROUSEL.length - 1
+        const newInx = isLastSlide ? 0 : curInx + 1
+        setCurInx(newInx)
+    }
     return (
-        <div>Slider</div>
+        <div className='carousel'>
+            <div className="container">
+                <div className="carousel__item" style={{ background: `url(${CAROUSEL[curInx]}) no-repeat center/cover` }}></div>
+                <button className="carousel__btn carousel__btn1" onClick={() => prevSlide()}><BsChevronCompactLeft /></button>
+                <button className="carousel__btn carousel__btn2" onClick={() => nextSlide()}><BsChevronCompactRight /></button>
+            </div>
+        </div>
     )
 }
 
