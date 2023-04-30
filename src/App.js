7fd6c8a3ef6c14fe5ae7efcb453d27a584e9c49d
Wertualistic <abduxalilovmuhammadali1@gmail.com>
2023-04-30 10:42:25 +0500

    React Toastify qoshildi

--- src/App.js
+++ src/App.js
@@ -10,16 +10,19 @@ import SinglePage from './router/single-page/SinglePage';
 import Comments from './components/comments/Comments';
 import UserProfile from './router/user-profile/UserProfile';
 import AddPost from './components/AddPost/AddPost';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <ToastContainer />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/profile' element={<UserProfile />} />
         <Route path='/products/:id' element={<SinglePage />} />
-        <Route path='/add' element={<AddPost />} />
+        <Route path='/add' element={<AddPost toast={toast} />} />
         <Route path='/login' element={<SignIn />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/liked' element={<Liked />} />
